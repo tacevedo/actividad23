5ce42500b17db61a05f964d68f721e4c350fd477
tam <tamara.acevedoavila@gmail.com>
2018-06-06 20:45:01 -0400

    carga parte plantilla

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,6 +7,19 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'css')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'img')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'js')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'bootstrap', 'css')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'bootstrap', 'js')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'elegant-font')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'font-awesome', 'css')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'ico')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'ultimate-flat-social-icons', 'ultm-css')
+Rails.application.config.assets.precompile += %w( */*.jpg,   */*.png)
+# Rails.application.config.assets.precompile += %w( testimonials/*.jpg )
+Rails.application.config.assets.precompile += %w( *.jpg, *.png )
+
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
