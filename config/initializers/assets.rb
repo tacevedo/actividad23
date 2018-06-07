6e49f5a0c4b16bad6391b7e86c2ca733b549a83f
tam <tamara.acevedoavila@gmail.com>
2018-06-06 20:47:57 -0400

    carga parte plantilla

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -16,7 +16,7 @@ Rails.application.config.assets.paths << Rails.root.join('vendor', 'elegant-font
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'font-awesome', 'css')
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'ico')
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'ultimate-flat-social-icons', 'ultm-css')
-Rails.application.config.assets.precompile += %w( */*.jpg,   */*.png)
+Rails.application.config.assets.precompile += %w( */*.jpg)
 # Rails.application.config.assets.precompile += %w( testimonials/*.jpg )
 Rails.application.config.assets.precompile += %w( *.jpg, *.png )
 
